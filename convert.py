e78e8759375608f9b528af06b7fb2e1907ab423c
Adrien <winy95battle@gmail.com>
2017-02-09 19:54:03 +0100

    Add arguments management and output handling

--- convert.py
+++ convert.py
@@ -1,4 +1,8 @@
 import csv
+import json
+import argparse
+import sys
+
 
 def csv2dict(data, delimiter):
     reader = csv.DictReader(data, delimiter=delimiter)
@@ -7,7 +11,17 @@ def csv2dict(data, delimiter):
     for row in reader:
         output.append(row)
 
-    print(output)
+    return json.dumps(output)
+def handle_result(result, output):
+    if output == 'stdout':
+        print(result)
+        return None
+
+    f = open(output, 'w')
+    f.write(result)
+    f.close()
+
+    return None
 
 def transform_csv(file_to_transform, from_format='csv', to_format='dict', **kwargs):
     f = open(file_to_transform, 'r')
@@ -21,11 +35,18 @@ def transform_csv(file_to_transform, from_format='csv', to_format='dict', **kwar
         delimiter = kwargs.get('delimiter')
         if not delimiter:
             raise Exception('No delimiter')
-        correspondance[from_format][to_format](f, delimiter=delimiter)
-
+        result = correspondance[from_format][to_format](f, delimiter=delimiter)
     f.close()
 
+    handle_result(result, kwargs['output_file'])
+
 if __name__ == '__main__':
-    file_path = '/tmp/toto.csv'
-    delimiter = ','
-    transform_csv(file_path, delimiter=delimiter)
+    parser = argparse.ArgumentParser(description='Convert a file to an other type')
+    parser.add_argument('--file', help='file to convert')
+    parser.add_argument('--input-format', default='csv', help='Input format')
+    parser.add_argument('--output-format', default='json', help='Output format')
+    parser.add_argument('--output-file', default='stdout', help='Output file')
+    parser.add_argument('--delimiter', default=',', help='delimiter of the csv')
+    args = parser.parse_args()
+
+    transform_csv(args.file, delimiter=args.delimiter, output_file=args.output_file)
