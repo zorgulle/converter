44d7eb9f343603a8e0fba8be1cc45c9d27ae801f
Adrien <winy95battle@gmail.com>
2017-02-09 20:00:57 +0100

    Lint code

--- convert.py
+++ convert.py
@@ -1,10 +1,14 @@
+"""This module convert file to another filetype
+Available convertion is CSV to JSON
+"""
 import csv
 import json
 import argparse
-import sys
 
 
 def csv2dict(data, delimiter):
+    """CSV to json convertion
+    """
     reader = csv.DictReader(data, delimiter=delimiter)
     output = []
 
@@ -12,19 +16,25 @@ def csv2dict(data, delimiter):
         output.append(row)
 
     return json.dumps(output)
+
 def handle_result(result, output):
+    """Handle ouputing of the result
+    """
     if output == 'stdout':
         print(result)
         return None
 
-    f = open(output, 'w')
-    f.write(result)
-    f.close()
+    f_to_write = open(output, 'w')
+    f_to_write.write(result)
+    f_to_write.close()
 
     return None
 
-def transform_csv(file_to_transform, from_format='csv', to_format='dict', **kwargs):
-    f = open(file_to_transform, 'r')
+def convert(file_to_transform, from_format='csv', to_format='dict', **kwargs):
+    """Convert type to an other type
+    """
+
+    f_to_convert = open(file_to_transform, 'r')
     correspondance = {
         'csv': {
             'dict': csv2dict
@@ -35,18 +45,18 @@ def transform_csv(file_to_transform, from_format='csv', to_format='dict', **kwar
         delimiter = kwargs.get('delimiter')
         if not delimiter:
             raise Exception('No delimiter')
-        result = correspondance[from_format][to_format](f, delimiter=delimiter)
-    f.close()
+        result = correspondance[from_format][to_format](f_to_convert, delimiter=delimiter)
+    f_to_convert.close()
 
     handle_result(result, kwargs['output_file'])
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert a file to an other type')
-    parser.add_argument('--file', help='file to convert')
-    parser.add_argument('--input-format', default='csv', help='Input format')
-    parser.add_argument('--output-format', default='json', help='Output format')
-    parser.add_argument('--output-file', default='stdout', help='Output file')
-    parser.add_argument('--delimiter', default=',', help='delimiter of the csv')
-    args = parser.parse_args()
+    PARSER = argparse.ArgumentParser(description='Convert a file to an other type')
+    PARSER.add_argument('--file', help='file to convert')
+    PARSER.add_argument('--input-format', default='csv', help='Input format')
+    PARSER.add_argument('--output-format', default='json', help='Output format')
+    PARSER.add_argument('--output-file', default='stdout', help='Output file')
+    PARSER.add_argument('--delimiter', default=',', help='delimiter of the csv')
+    ARGS = PARSER.parse_args()
 
-    transform_csv(args.file, delimiter=args.delimiter, output_file=args.output_file)
+    convert(ARGS.file, delimiter=ARGS.delimiter, output_file=ARGS.output_file)
